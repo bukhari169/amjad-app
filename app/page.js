95c46cb2bfb7c86369219ecc8817201b994784d6
bukhari169 <amjad69uk@gmail.com>
2026-04-19 13:35:26 +0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>🚀 Amjad App is Live</h1>
+      <p>Your project is now working correctly.</p>
+    </main>
+  );
+}
